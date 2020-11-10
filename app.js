bfa93e1f885aa082690151dc342adf5846d7bc64
Rafael Aquino <rafaaquino28@gmail.com>
2020-11-10 16:41:38 -0300

    conexão com websocket.io

--- app.js
+++ app.js
@@ -3,7 +3,20 @@
 var app = require('./config/server');
 
 /* Porta de escuta*/
-app.listen(80, function(){
+var server = app.listen(80, function(){
 
 	console.log("Servidor On");
 })
+
+
+var io = require('socket.io').listen(server);
+
+
+io.on('connection', function(socket){
+
+	console.log('usuario conectado');
+
+	socket.on('disconnect', function(){
+		console.log('usuario desconectado');
+	})
+});
